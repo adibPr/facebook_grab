f488e1691676c86be83936a4d25bbe13c476a23b
AdibPr <pratama.adib@gmail.com>
2018-08-25 15:33:01 +0200

    Rechange the way search work, use direct URL rather than UI

--- main.py
+++ main.py
@@ -19,8 +19,14 @@ class FacebookGrab (object) :
 
 
     def __init__ (self, **kwargs) : 
-        self.keyword = ['jokowi', 'prabowo', '2019gantipresiden']
-        self.driver = webdriver.Firefox ()
+        self.keywords = ['jokowi', 'prabowo', '2019gantipresiden']
+
+        # set profile 
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference ('dom.webnotifications.enabled', False)
+        self.driver = webdriver.Firefox (fp)
+
+        # load account
         self.load_account (kwargs.get ('path_account', './account.ini'))
 
     def do_login (self) : 
@@ -40,10 +46,35 @@ class FacebookGrab (object) :
         config.read (path)
         self.account = config['account']
 
+    def process (self) : 
+        # login
+        self.do_login ()
+
+        # change address to search query url 
+        self.driver.get (self.get_search_address (self.keywords[0])) 
+
+        self.get_posts ()
+
+    def get_search_address (self, addr) : 
+        # for public post
+        PUBLIC_STORIES = 'https://www.facebook.com/search/str/{}/stories-keyword/stories-public'
+        # for friends
+        FRIEND_STORIES = 'https://www.facebook.com/search/str/{}/keywords_blended_posts?filters_rp_author=stories-feed-friends'
+        return FRIEND_STORIES.format ('+'.join (addr.split (' ')))
+
+    def get_posts (self) :
+        elemets = self.driver.find_elements_by_class_name ('userContent')
+        print (len (elemets))
+        for e in elemets : 
+            see_more = e.find_elements_by_class_name ("see_more_link_inner")
+            if see_more : 
+                see_more[0].click ()
+            print (e.text)
+        
 
 def test_1 () : 
     FG = FacebookGrab ()
-    FG.do_login ()
+    FG.process ()
 
 if __name__ == '__main__' : 
     test_1 ()
